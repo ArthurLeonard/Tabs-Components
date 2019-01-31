6b66d4af91227465407052f17404a962fcd2c1b5
ArthurLeonard <ialeonard@ucdavis.edu>
2019-01-31 15:54:03 -0800

    fixed dropdown menu and add event listener in Tabs.js

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -2,22 +2,25 @@
 class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
-    // this.element;
-    
+    this.element = element;
+    console.log(this.element);
     // Get the custom data attribute on the Link
-    // this.data;
-    
+     this.data = this.element.dataset.tab;
+     console.log(this.data);
     // Using the custom data attribute get the associated Item element
-    // this.itemElement;
-    
-    // Using the Item element, create a new instance of the TabItem class
-    // this.tabItem;
-    
-    // Add a click event listener on this instance, calling the select method on click
+    this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
+    console.log(this.itemElement);
 
+    // Using the Item element, create a new instance of the TabItem class
+    this.tabItem = new TabItem(this.itemElement);
+
+    // Add a click event listener on this instance, calling the select method on click
+    this.element.addEventListener('click', () => this.select() );
+  
   };
 
   select() {
+    console.log("in select function");
     // Get all of the elements with the tabs-link class
     // const links;
 
@@ -35,7 +38,7 @@ class TabLink {
 class TabItem {
   constructor(element) {
     // Assign this.element to the passed in element
-    // this.element;
+    this.element = element;
   }
 
   select() {
@@ -59,4 +62,7 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+const links = document.querySelectorAll('.tabs-link');
+
+links.forEach( link => new TabLink(link));
+
