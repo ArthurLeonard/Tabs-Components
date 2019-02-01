f2c93047ce70e26f23ad0394b39dd9c9028b01ca
ArthurLeonard <ialeonard@ucdavis.edu>
2019-01-31 17:43:09 -0800

    cleaned up console logs and added new tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -3,13 +3,13 @@ class TabLink {
   constructor(element) {
     // Assign this.element to the passed in DOM element
     this.element = element;
-    console.log(this.element);
+    
     // Get the custom data attribute on the Link
      this.data = this.element.dataset.tab;
-     console.log(this.data);
+     
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab='${this.data}']`);
-    console.log(this.itemElement);
+    
 
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
@@ -20,17 +20,17 @@ class TabLink {
   };
 
   select() {
-    console.log("in select function");
+    
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll('.tabs-link');
-    console.log(links);
+    
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
     Array.from(links).forEach(lnk => lnk.classList.remove('tabs-link-selected'));
-    console.log(links);
+    
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
-    console.log(this.element);
+    
     // Call the select method on the item associated with this link
     this.tabItem.select();
   }
@@ -51,7 +51,7 @@ class TabItem {
 
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-item-selected');
-    console.log(this.element);
+   
   }
 }
 
